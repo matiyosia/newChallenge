fbc458a727070796af6dbb725f82358798ad7423
MatiasYosia <matiiyosia@gmail.com>
2023-12-16 18:38:13 -0300

    validation edit email

---
+++ src/components/Alert/Alert.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-alert
+    style="
+      position: absolute;
+      left: 50%;
+      top: 30%;
+      z-index: 9999;
+      transform: translate(-50%, -50%);
+    "
+    density="compact"
+    :type="type"
+    :title="title"
+    :text="text"
+  ></v-alert>
+</template>
+
+<script>
+export default {
+  props: {
+    type: String, 
+    title: String, 
+   text: String, 
+  },
+};
+</script>
